cfa07a44efa0ee3a42b061a80887082fbe8cfca5
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-10-14 11:43:37 -0500

    feat: add interfaces and usecases related to delete documents

--- internal/application/interfaces/object_storage.go
+++ internal/application/interfaces/object_storage.go
@@ -9,4 +9,5 @@ type ObjectStorage interface {
 	Put(ctx context.Context, body io.Reader, objectKey, contentType string) error
 	PublicURL(objectKey string) string
 	Bucket() string
+	Delete(ctx context.Context, objectKey string) error
 }
