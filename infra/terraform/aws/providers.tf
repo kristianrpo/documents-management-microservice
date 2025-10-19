06a43fbad7c1f2ece4a32f8b751f7d5766eb2fdc
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-10-19 00:27:03 -0500

    fix: correct error elements from terraform

--- infra/terraform/aws/providers.tf
+++ infra/terraform/aws/providers.tf
@@ -15,23 +15,22 @@ provider "aws" {
   region = var.aws_region
 }
 
+# EKS cluster data (for providers auth)
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
 # Kubernetes provider (uses EKS cluster created by this module)
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      module.eks.cluster_name,
-      "--region",
-      var.aws_region
-    ]
-  }
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 # Alias for Helm provider linkage
@@ -57,19 +56,8 @@ provider "kubernetes" {
 # Helm provider (installs charts in EKS) - v3 syntax using inline kubernetes config
 provider "helm" {
   kubernetes = {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = [
-        "eks",
-        "get-token",
-        "--cluster-name",
-        module.eks.cluster_name,
-        "--region",
-        var.aws_region
-      ]
-    }
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
   }
 }
